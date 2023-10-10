ac1bc60c26f5065095bb2d7e8d7809ffb571a74a
KamranMoqadam <kamranmoqadam97@gmail.com>
2023-10-11 00:06:49 +0330

    add file

---
+++ src/js/init.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+    document.getElementById("Preloader").style.display = "none";
+};
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerButton = document.querySelector("[data-collapse-toggle='navbar-hamburger']");
+    const navbarMenu = document.getElementById("navbar-hamburger");
+
+    hamburgerButton.addEventListener("click", function () {
+        navbarMenu.classList.toggle("hidden");
+        const expanded = navbarMenu.getAttribute("aria-expanded") === "true" || false;
+        navbarMenu.setAttribute("aria-expanded", !expanded);
+    });
+});
