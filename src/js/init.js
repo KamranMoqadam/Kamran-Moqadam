3179b69b201606c83980dfa95a865baa8ae61783
KamranMoqadam <kamranmoqadam97@gmail.com>
2023-10-13 15:58:37 +0330

    bug fix preloader

--- src/js/init.js
+++ src/js/init.js
@@ -1,8 +1,10 @@
+document.getElementById("About").style.display = "none";
+document.getElementById("Contact").style.display = "none";
+// document.getElementById("Service").style.display = "none";
+// document.getElementById("Portfolio").style.display = "none";
+// document.getElementById("News").style.display = "none";
 window.onload = function () {
-
     document.getElementById("Preloader").style.display = "none";
-    document.getElementById("About").style.display = "none";
-    document.getElementById("Contact").style.display = "none";
 };
 document.addEventListener("DOMContentLoaded", function () {
     const hamburgerButton = document.querySelector("[data-collapse-toggle='navbar-hamburger']");
